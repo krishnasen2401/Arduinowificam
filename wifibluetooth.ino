c7c6156638f0c89854ab47bf458c7cc11a8f1354
Lingam mohan Krishna sen <2401krishnasen@gmail.com>
2024-09-29 01:39:04 +0530

    Add files via upload
    Using SPP bluetooth will stop before started and will start again only after client connects

--- wifibluetooth.ino
+++ wifibluetooth.ino
@@ -7,9 +7,9 @@
 #include "camera_pins.h"
 #include "esp_http_server.h"
 #include "esp_log.h"
-
-bool serverStarted = false;
-
+#include "fb_gfx.h"
+#include "img_converters.h"
+#define PART_BOUNDARY "123456789000000000000987654321"
 
 // Check if Bluetooth is available
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -22,17 +22,21 @@ bool serverStarted = false;
 #endif
 
 BluetoothSerial SerialBT;
-
-#define PART_BOUNDARY "123456789000000000000987654321"
-static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+bool stop_server_flag = false;
 
 void sendStringOverBT(const String& data) {
   const char* charData = data.c_str();
   SerialBT.write((const uint8_t*)charData, strlen(charData)); // Sends the string over Bluetooth
   Serial.println("Data sent over Bluetooth:");
   Serial.println(data);
+}
+
+void endBluetooth() {
+  Serial.println("Stopping Bluetooth...");
+  SerialBT.flush();
+  SerialBT.disconnect();
+  SerialBT.end();
+  Serial.println("Bluetooth stopped.");
 }
 
 void disconnectWifi(){
@@ -75,6 +79,7 @@ void connectWifi(const char *ssid,const char *password){
     delay(tryDelay);
     if (numberOfTries <= 0) {
       Serial.print("[WiFi] Failed to connect to WiFi!");
+      sendStringOverBT("[WiFi] Failed to connect to WiFi!");
       // Use disconnect function to force stop trying to connect
       WiFi.disconnect();
       return;
@@ -83,16 +88,6 @@ void connectWifi(const char *ssid,const char *password){
     }
   }    
   }
-
-httpd_handle_t stream_httpd = NULL;
-
-// Task handle for the camera setup task
-TaskHandle_t cameraSetupTaskHandle = NULL;
-
-void setupCameraTask(void *pvParameters) {
-  setupCamera();
-  vTaskDelete(NULL); // Delete the task after setup is complete
-}
 
 void setupCamera() {
     Serial.printf("Free heap before camera init: %d\n", heap_caps_get_free_size(MALLOC_CAP_8BIT));
@@ -115,7 +110,7 @@ void setupCamera() {
   config.pin_sccb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 10000000; //20000000
+  config.xclk_freq_hz = 20000000; //20000000 10000000
   config.pixel_format = PIXFORMAT_JPEG;  // for streaming
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM; //
@@ -143,100 +138,42 @@ void setupCamera() {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
-
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
 Serial.printf("Free heap after camera init: %d\n", heap_caps_get_free_size(MALLOC_CAP_8BIT));
-
 }
 
-esp_err_t index_handler(httpd_req_t *req) {
-  httpd_resp_send(req, "Hello, world!", HTTPD_RESP_USE_STRLEN);
-  return ESP_OK;
-}
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
-void startMinimalServer() {
-  if (serverStarted) {
-    Serial.println("Server is already running");
-    return;
-  }
-  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-  config.server_port = 8080; // Change port number
-  config.core_id = 1; // Pin task to CPU 1
-  config.stack_size = 8192; // Increase stack size
-  config.task_priority = tskIDLE_PRIORITY + 5; // Set appropriate priority
+httpd_handle_t stream_httpd = NULL;
 
-  httpd_uri_t index_uri = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = index_handler,
-    .user_ctx  = NULL
-  };
-esp_log_level_set("httpd", ESP_LOG_DEBUG);
-  esp_err_t err = httpd_start(&stream_httpd, &config);
-  if (err == ESP_OK) {
-    httpd_register_uri_handler(stream_httpd, &index_uri);
-    Serial.println("Minimal server started successfully");
-  } else {
-    Serial.printf("Error starting minimal server: %s\n", esp_err_to_name(err));
-  }
-}
-
-void stopMinimalServer() {
-  if (serverStarted) {
-    httpd_stop(stream_httpd);
-    serverStarted = false;
-    Serial.println("Minimal server stopped");
-  }
-}
-
-
-// void startCameraServer1() {
-//   httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-//   config.server_port = 8080; // Change port number
-//   // config.stack_size = 16384; // Increase stack size
-//   // config.task_priority = tskIDLE_PRIORITY + 10; // Increase task priority
-//   config.core_id = 0;
-//   config.lru_purge_enable = true;
-
-//   httpd_uri_t index_uri = {
-//     .uri       = "/",
-//     .method    = HTTP_GET,
-//     .handler   = stream_handler,
-//     .user_ctx  = NULL
-//   };
-//   sendStringOverBT("Starting web server http://" + WiFi.localIP().toString() + ":" + String(config.server_port));
-// esp_err_t err = httpd_start(&stream_httpd, &config);
-// if (err == ESP_OK) {
-//   httpd_register_uri_handler(stream_httpd, &index_uri);
-//   Serial.println("Server started successfully");
-// } else {
-//   Serial.printf("Error starting server: %s\n", esp_err_to_name(err));
-// }
-// }
-
-static esp_err_t stream_handler(httpd_req_t *req) {
-  camera_fb_t *fb = NULL;
+static esp_err_t stream_handler(httpd_req_t *req){
+  camera_fb_t * fb = NULL;
   esp_err_t res = ESP_OK;
   size_t _jpg_buf_len = 0;
-  uint8_t *_jpg_buf = NULL;
-  char part_buf[64];
-
+  uint8_t * _jpg_buf = NULL;
+  char * part_buf[64];
+  Serial.println("Client connected");
   res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-  if (res != ESP_OK) {
+  if(res != ESP_OK){
     return res;
   }
 
-  while (true) {
+  while(true){
     fb = esp_camera_fb_get();
     if (!fb) {
       Serial.println("Camera capture failed");
       res = ESP_FAIL;
     } else {
-      if (fb->width > 400) {
-        if (fb->format != PIXFORMAT_JPEG) {
+      if(fb->width > 400){
+        if(fb->format != PIXFORMAT_JPEG){
           bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
           esp_camera_fb_return(fb);
           fb = NULL;
-          if (!jpeg_converted) {
+          if(!jpeg_converted){
             Serial.println("JPEG compression failed");
             res = ESP_FAIL;
           }
@@ -246,29 +183,75 @@ static esp_err_t stream_handler(httpd_req_t *req) {
         }
       }
     }
-    if (res == ESP_OK) {
+    if(res == ESP_OK){
       size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
       res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
     }
-    if (res == ESP_OK) {
+    if(res == ESP_OK){
       res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
     }
-    if (res == ESP_OK) {
+    if(res == ESP_OK){
       res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
     }
-    if (fb) {
+    if(fb){
       esp_camera_fb_return(fb);
       fb = NULL;
       _jpg_buf = NULL;
-    } else if (_jpg_buf) {
+    } else if(_jpg_buf){
       free(_jpg_buf);
       _jpg_buf = NULL;
     }
-    if (res != ESP_OK) {
+    if(res != ESP_OK){
       break;
     }
+    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
   }
+  Serial.printf("Client disconnected");
+  stop_server_flag = true;
   return res;
+}
+
+void startCameraServer(){
+  endBluetooth();
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.server_port = 80;
+  config.lru_purge_enable=true;
+  config.core_id=0;
+
+  httpd_uri_t index_uri = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = stream_handler,
+    .user_ctx  = NULL
+  };
+  
+  //Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+esp_err_t err = httpd_start(&stream_httpd, &config);
+if (err == ESP_OK) {
+    Serial.println("Web server started successfully");
+    httpd_register_uri_handler(stream_httpd, &index_uri);
+} else {
+    Serial.print("Failed to start web server, error code: ");
+    Serial.println(err);
+}
+}
+
+void stopCameraServer() {
+    if (stream_httpd != NULL) {
+        Serial.println("Stopping camera server...");
+        esp_err_t err = httpd_stop(stream_httpd);
+        Serial.println("After httpd_stop call");  // Debugging statement
+        if (err == ESP_OK) {
+            stream_httpd = NULL;
+            Serial.println("Camera server stopped successfully");
+            SerialBT.begin("m5stackesp32Cam");  // Bluetooth device name
+            Serial.println("Restarted Bluetooth");
+        } else {
+            Serial.printf("Failed to stop camera server: %s\n", esp_err_to_name(err));
+        }
+    } else {
+        Serial.println("Camera server handle is NULL, cannot stop server.");
+    }
 }
 
 void setup() {
@@ -283,18 +266,41 @@ void setup() {
   }
   SerialBT.begin("m5stackesp32Cam");  //Bluetooth device name
   Serial.printf("The device with name m5stackesp32Cam is started.\nNow you can pair it with Bluetooth!\n");
-xTaskCreatePinnedToCore(
-      setupCameraTask,    // Task function
-      "CameraSetupTask",  // Task name
-      4096,               // Stack size
-      NULL,               // Task parameter
-      1,                  // Task priority
-      &cameraSetupTaskHandle, // Task handle
-      0                   // CPU core
-    );
+  setupCamera();
+  // WiFi.mode(WIFI_STA);
+
+  // Register the SPP callback
+    // esp_spp_register_callback(sppCallback);
+    // esp_spp_init(ESP_SPP_MODE_CB);
+}
+
+// void sppCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+//     switch (event) {
+//         case ESP_SPP_SRV_OPEN_EVT:  // Client connected
+//             Serial.println("Client connected");
+//             printDeviceAddress(param->srv_open.rem_bda);
+//             break;
+//         case ESP_SPP_CLOSE_EVT:  // Client disconnected
+//             Serial.println("Client disconnected");
+//             break;
+//         default:
+//             break;
+//     }
+// }
+
+void printDeviceAddress(esp_bd_addr_t bda) {
+    char bda_str[18];
+    snprintf(bda_str, sizeof(bda_str), "%02X:%02X:%02X:%02X:%02X:%02X",
+             bda[0], bda[1], bda[2], bda[3], bda[4], bda[5]);
+    Serial.printf("Connected device address: %s\n", bda_str);
 }
 
 void loop() {
+   if (stop_server_flag) {
+        stopCameraServer();
+        stop_server_flag = false;  // Reset the flag
+    }
+
   if (SerialBT.available()) {
     String jsonString = SerialBT.readStringUntil('\n'); // Read until newline character
     jsonString.trim();
@@ -315,13 +321,11 @@ void loop() {
     disconnectWifi();
     }else if (strcmp(command, "startcamera") == 0) {
       if (WiFi.status() == WL_CONNECTED) {
-        startMinimalServer();
+            startCameraServer();
       } else {
-        Serial.println("WiFi not connected. Cannot start server.");
+        Serial.println("WiFi not connected or server already running.");
       }
-    } else if (strcmp(command, "stopcamera") == 0) {
-      stopMinimalServer();
-    }else {
+    } else {
     Serial.println("Command does not match");
   }
   }
